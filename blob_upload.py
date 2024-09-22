69cbb2a53aad541055e473709ebb5bb98cade6b4
Swati Shahi <swatishahi.555@gmail.com>
2024-09-22 13:47:11 +0200

    changes added to upload file to blob

--- blob_upload.py
+++ blob_upload.py
@@ -1,7 +1,11 @@
 from azure.storage.blob import BlobServiceClient
 from azure.storage.blob import ContainerClient
+from azure.storage.blob import BlobClient
+import os
 
 container_name = "raw"
+blob_name = "tourism_dataset.csv"
+file_path = "https://dataengineerv1.blob.core.windows.net/raw/tourism_dataset.csv"
 
 # Azure Blob Storage connection string
 connection_string = "https://dataengineerv1.blob.core.windows.net/raw"
@@ -18,3 +22,13 @@ if not container_client.exists():
     print(f"Container '{container_name}' created.")
 else:
     print(f"Container '{container_name}' already exists.")
+
+ # Ensure file exists
+if not os.path.exists(file_path):
+    raise FileNotFoundError(f"The file {file_path} does not exist.")   
+
+# Upload the file
+blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+with open(file_path, "rb") as file:
+    blob_client.upload_blob(file)
+    print(f"File '{blob_name}' uploaded to container '{container_name}'.")
