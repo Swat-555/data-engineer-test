0d89f0a4af82a87f20e6b01215893da0740284c0
Swati Shahi <swatishahi.555@gmail.com>
2024-09-22 14:20:27 +0200

    creating client

--- deploy-vm-with-nic.py
+++ deploy-vm-with-nic.py
@@ -6,3 +6,14 @@ from azure.mgmt.compute.models import (HardwareProfile, NetworkProfile,
                                        OSProfile, VirtualMachine, StorageProfile,
                                        OSDisk, ImageReference)
 
+# Set up authentication
+credential = DefaultAzureCredential()
+
+subscription_id = "aee8556f-d2fd-4efd-a6bd-f341a90fa76e"
+resource_group_name = "Data_Engineer"
+location = "westeu"
+
+# Create clients
+resource_client = ResourceManagementClient(credential, subscription_id)
+compute_client = ComputeManagementClient(credential, subscription_id)
+network_client = NetworkManagementClient(credential, subscription_id)
