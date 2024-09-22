a006f9e0eac6885e271c3f0e1a93a34d280ae470
Swati Shahi <swatishahi.555@gmail.com>
2024-09-22 14:51:05 +0200

    added changes to create vnet and subnet

--- network-setup.py
+++ network-setup.py
@@ -10,3 +10,33 @@ RESOURCE_GROUP = 'Data_Engineer'
 
 # Authenticate using DefaultAzureCredential
 credential = DefaultAzureCredential()
+
+from azure.mgmt.network.models import VirtualNetwork, Subnet
+
+network_client = NetworkManagementClient(credential, SUBSCRIPTION_ID)
+
+# Create VNet
+vnet_params = {
+    "location": "westeu",
+    "address_space": {"address_prefixes": ["10.0.0.0/16"]}
+}
+vnet = network_client.virtual_networks.begin_create_or_update(
+    RESOURCE_GROUP,
+    'swatishahi',
+    vnet_params
+).result()
+
+# Create Subnet
+subnet_params = {
+    "address_prefix": "10.0.0.0/24"
+}
+subnet = network_client.subnets.begin_create_or_update(
+    RESOURCE_GROUP,
+    'swatishahi',
+    'swati-subnet',
+    subnet_params
+).result()
+
+print(f"VNet created: {vnet.id}")
+print(f"Subnet created: {subnet.id}")
+
