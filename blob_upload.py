06cfbff01f40ac9f0c640eb56c9391276fa16186
Swati Shahi <swatishahi.555@gmail.com>
2024-09-22 13:28:25 +0200

    Initialized BLOB setup

--- blob_upload.py
+++ blob_upload.py
@@ -1,7 +1,7 @@
 from azure.storage.blob import BlobServiceClient
 
 # Azure Blob Storage connection string
-connection_string = "<Your_Azure_Storage_Account_Connection_String>"
+connection_string = "https://dataengineerv1.blob.core.windows.net/raw"
 
 # Initialize BlobServiceClient
 blob_service_client = BlobServiceClient.from_connection_string(connection_string)
