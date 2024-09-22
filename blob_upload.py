4f36b0253d9eaefdeb5330e65f981daeee949954
Swati Shahi <swatishahi.555@gmail.com>
2024-09-22 13:39:42 +0200

    Step 2: Check if the container exists and create it if needed

--- blob_upload.py
+++ blob_upload.py
@@ -1,4 +1,7 @@
 from azure.storage.blob import BlobServiceClient
+from azure.storage.blob import ContainerClient
+
+container_name = "raw"
 
 # Azure Blob Storage connection string
 connection_string = "https://dataengineerv1.blob.core.windows.net/raw"
@@ -7,3 +10,11 @@ connection_string = "https://dataengineerv1.blob.core.windows.net/raw"
 blob_service_client = BlobServiceClient.from_connection_string(connection_string)
 
 print("Blob service client initialized.")
+
+# Check if container exists, if not, create it
+container_client = blob_service_client.get_container_client(container_name)
+if not container_client.exists():
+    container_client.create_container()
+    print(f"Container '{container_name}' created.")
+else:
+    print(f"Container '{container_name}' already exists.")
