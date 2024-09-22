cff7e8f30e381bd9bb4714f2358c21abf84d4ed5
Swati Shahi <swatishahi.555@gmail.com>
2024-09-22 14:53:02 +0200

    added changes to create network interface card

--- network-setup.py
+++ network-setup.py
@@ -3,6 +3,8 @@ from azure.mgmt.resource import ResourceManagementClient
 from azure.mgmt.network import NetworkManagementClient
 from azure.mgmt.compute import ComputeManagementClient
 from azure.mgmt.storage import StorageManagementClient
+from azure.mgmt.network.models import VirtualNetwork, Subnet
+from azure.mgmt.network.models import NetworkInterface
 
 # Define subscription ID and resource group
 SUBSCRIPTION_ID = 'aee8556f-d2fd-4efd-a6bd-f341a90fa76e'
@@ -10,8 +12,6 @@ RESOURCE_GROUP = 'Data_Engineer'
 
 # Authenticate using DefaultAzureCredential
 credential = DefaultAzureCredential()
-
-from azure.mgmt.network.models import VirtualNetwork, Subnet
 
 network_client = NetworkManagementClient(credential, SUBSCRIPTION_ID)
 
@@ -39,4 +39,22 @@ subnet = network_client.subnets.begin_create_or_update(
 
 print(f"VNet created: {vnet.id}")
 print(f"Subnet created: {subnet.id}")
+
+nic_params = {
+    "location": "eastus",
+    "ip_configurations": [{
+        "name": "ipconfig1",
+        "subnet": {"id": subnet.id},
+        "private_ip_allocation_method": "Dynamic"
+    }]
+}
+
+nic = network_client.network_interfaces.begin_create_or_update(
+    RESOURCE_GROUP,
+    'NIc-SwatiShahi',
+    nic_params
+).result()
+
+print(f"NIC created: {nic.id}")
+
 
